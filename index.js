7007359ad9e0f8fdcc9b12f6986acd6cdbc500f7
Daniel <danielmadalena@msn.com>
2018-02-09 15:45:52 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var bodyParser = require("body-parser");
+var path = require("path");
+// var encomendas = require("./routes/encomendas");
+// var gestao = require("./routes/gestao");
+// var auth = require("./middlewares/auth");
+var app = express();
+
+app.set("port", (process.env.PORT || 3000));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+// app.use(auth.router);
+
+// app.get('*', function(req, res, next){
+//    if(req.path.includes('log')){
+//     next();
+//    }else{
+//       auth.isAuthenticated(req, res, next);
+//    }
+// });
+
+// Get Static Path
+app.use(express.static(path.join(__dirname, "public")));
+// app.use("/", encomendas);
+// app.use("/gestao", gestao.router);
+
+app.get("/",(req, res)=>{
+  res.send("HELLO")
+})
+
+app.listen(app.get("port"), function() {
+  console.log("App is listenning o port 3000");
+});
---
+++ public/log/js/login.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  $(".btn-container").on("click", function() {
+    var email = $("#email").val();
+    var pass = $("#pass").val();
+    var credentials = {
+      "email": email,
+      "password": pass
+    }
+
+    $(".btn-container h4").addClass("hide");
+    $(".sub-loader").addClass("displayLoader");
+
+    $.ajax({
+      type: "POST",
+      url: "/login",
+      data: credentials,
+      success: function(res) {
+        $(".btn-container h4").removeClass("hide");
+        $(".sub-loader").removeClass("displayLoader");
+        if(res.error){
+          $(".error").html(res.msg);
+          $(".error-container").addClass("entrance").on("animationend",function(){
+            $(this).removeClass("entrance");
+          });
+        }else{
+          window.location.href = "../index.html";
+          // $(".welcome-container").addClass("welcome-entrance").on("animationend",function(){
+          //   window.location.href = "../index.html";
+          // });
+        }
+      }
+    });
+
+  });
+
+
+})

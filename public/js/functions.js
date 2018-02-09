483bbf8e11fc804a95308238bd7bdd3fcdaab339
Daniel <danielmadalena@msn.com>
2018-02-09 17:12:25 +0000

    navbar menu animation

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,4 +1,41 @@
 $(document).ready(function(){
+  let menuClicked = 0
+  $(".menu-icon-container").click(()=>{
+    if(menuClicked){
+      menuClicked = 0
+      hideNav()
+    }else{
+      menuClicked = 1
+      showNav()
+    }
+    
+  });
   
-  
+  function showNav() {
+    $(".line-top").css({
+      "top": "50%",
+      "transform": "translate(-50%, -50%) rotate(-45deg)"
+    });
+    $(".line-center").css({
+      "opacity": 0
+    });
+    $(".line-bottom").css({
+      "top": "50%",
+      "transform": "translate(-50%, -50%) rotate(45deg)"
+    });
+  }
+
+  function hideNav() {
+    $(".line-top").css({
+      "top": "39%",
+      "transform": "translateX(-50%) rotate(0deg)"
+    });
+    $(".line-center").css({
+      "opacity": 1
+    });
+    $(".line-bottom").css({
+      "top": "61%",
+      "transform": "translateX(-50%) rotate(0deg)"
+    });
+  }
 })

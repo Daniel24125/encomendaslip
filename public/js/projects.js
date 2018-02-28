be9d100e656cf162b7a89c465e7428e8c4009bf2
Daniel <danielmadalena@msn.com>
2018-02-28 22:46:30 +0000

    Finishing Manegement Design

--- public/js/projects.js
+++ public/js/projects.js
@@ -1,20 +1,20 @@
 $(document).ready(function () {
-      let projNames, projData;
-      fetch('/getProjectsInfo')
-        .then(response => response.json())
-        .then(res => {
-          $(".loader").addClass("hideComponent")
-          projNames = Object.keys(res.data);
-          projData = res.data;
-          let years
-          projNames.map((item, index) => {
-            if (item != "Crédito") {
-              let currentProj = projData[item];
-              years = Object.keys(currentProj);
-              years.splice((Object.keys(currentProj).length - 4), Object.keys(currentProj).length)
-              years = years.reverse()
-              $(".cardOverflowContainter").append(`
-                <div ids="${item}" class="card">
+  let projNames, projData;
+  fetch('/getProjectsInfo')
+    .then(response => response.json())
+    .then(res => {
+      $(".loader").addClass("hideComponent")
+      projNames = Object.keys(res.data);
+      projData = res.data;
+      let years
+      projNames.map((item, index) => {
+        if (item != "Crédito") {
+          let currentProj = projData[item];
+          years = Object.keys(currentProj);
+          years.splice((Object.keys(currentProj).length - 4), Object.keys(currentProj).length)
+          years = years.reverse()
+          $(".cardOverflowContainter").append(`
+                <div id="${item}" class="card">
                 <div class="yearsContainer">                  
                 </div>
                 <span class="projectTitle">${item}</span>
@@ -23,17 +23,24 @@ $(document).ready(function () {
                   <span class="dataFim">${currentProj.DataFim}</span>
                 </div>
                 <div class="infoContainer">
-                  <div title="Orçamento Total" class="cardInfoContainer">
-                    <span class="infoCategory">OT: </span>
-                    <span class="otValue">${currentProj.OrcTotal}</span>€
+                  <div class="statsNumbersContainer">
+                    <div title="Orçamento Total" class="cardInfoContainer">
+                      <span class="infoCategory">OT: </span>
+                      <span class="otValue">${currentProj.OrcTotal}</span>€
+                    </div>
+                    <div title="Orçamento Total Disponível" class="cardInfoContainer">
+                      <span class="infoCategory">OTD: </span>
+                      <span class="otdValue">${currentProj.OrcTotalDisp}</span>€
+                    </div>
+                    <div title="Plafond Anual Atribuído" class="cardInfoContainer">
+                      <span class="infoCategory">PAA: </span>
+                      <span class="paaValue">${currentProj[years[0]].plafonds.PlafondAtribuido}</span>€
+                    </div>
                   </div>
-                  <div title="Orçamento Total Disponível" class="cardInfoContainer">
-                    <span class="infoCategory">OTD: </span>
-                    <span class="otdValue">${currentProj.OrcTotalDisp}</span>€
-                  </div>
-                  <div title="Plafond Anual Atribuído" class="cardInfoContainer">
-                    <span class="infoCategory">PAA: </span>
-                    <span class="otdValue">2413.92</span>€
+                  <div class="pad">
+                    <div title="Plafond Anual Displonível" class="cardInfoContainer">
+                      <span class="padValue">${currentProj[years[0]].plafonds.PlafondDisp}</span>€
+                    </div>
                   </div>
                 </div>
                 <div class="cardFooterContainer">
@@ -43,57 +50,87 @@ $(document).ready(function () {
               </div>
            
                 `);
-            }
-          });
-          $(".cardOverflowContainter").css("width", `${300*$(".card").length}px`)
-          years.map((y) => {
-            $(".yearsContainer").append(`
+        }
+      });
+
+      $(".cardOverflowContainter").css("width", `${300*$(".card").length}px`)
+      years.map((y) => {
+        $(".yearsContainer").append(`
                 <span class="year">${y}</span>
               `);
-          });
-          $(".year:first-child").addClass("activeYear")
-
-        });
-
-      $("body").on("click", ".year", function () {
-        $(this).parent().children().removeClass("activeYear");
-        $(this).addClass("activeYear")
       });
+      $(".year:first-child").addClass("activeYear");
+      $(".card:first-child").addClass("activeCard")
+      getStats($(".card:first-child").attr("id"), $(".card:first-child .activeYear").html())
+    });
 
+  $("body").on("click", ".year", function () {
+    $(this).parent().children().removeClass("activeYear");
+    $(this).addClass("activeYear");
+    let yearClicked = $(this).html()
+    let yearOfProject = $(this).parents().eq(1).attr("id")
+    $(`#${yearOfProject} .otdValue`).html(projData[yearOfProject].OrcTotalDisp)
+    $(`#${yearOfProject} .paaValue`).html(projData[yearOfProject][yearClicked].plafonds.PlafondAtribuido)
+    $(`#${yearOfProject} .padValue`).html(projData[yearOfProject][yearClicked].plafonds.PlafondDisp)
+  });
 
-      // GRAFICO
-      var ctx = document.getElementById('statsChart').getContext('2d');
-      var myBarChart = new Chart(ctx, {
-          type: 'bar',
-          data: {
-            labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
-            datasets: [{
-                label: "Cabimentado",
-                backgroundColor: "#2850a7",
-                data: [10000, 100, 200, 500, 1000, 4000, 10000, 100, 200, 500, 1000, 4000]
-              },
-              {
-                label: "Faturado",
-                backgroundColor: "#2ecc71",
-                data: [10000, 100, 200, 500, 1000, 4000, 10000, 100, 200, 500, 1000, 4000]
-              }
-            ]
-            },
-            options: {
-              legend: {
-                display: false
-              },
-              maintainAspectRatio: false,
-              scales: {
-                yAxes: [{
-                  ticks: {
-                    beginAtZero: true
-                  }
-                }]
-              }
+  $("body").on("click", ".carregarDados", function () {
+    let selectedProject = $(this).parents().eq(1).attr("id"), 
+    selectedYear = $(this).parents().eq(1).find(".activeYear").html();
+    console.log(selectedProject)
+    $(".activeCard").removeClass("activeCard")
+    $(this).parents().eq(1).addClass("activeCard")
+    getStats(selectedProject, selectedYear);
+  });
+
+  let ctx = document.getElementById('statsChart').getContext('2d');
+  let myBarChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+        datasets: [{
+            label: "Cabimentado",
+            backgroundColor: "#2850a7",
+          },
+          {
+            label: "Faturado",
+            backgroundColor: "#2ecc71",
+          }
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
             }
-          });
+          }]
+        }
+      }
+    });
 
-          myBarChart.data.datasets[0].data = [12000, 11000, 10000, 9000, 8000, 7000, 6000, 5000, 4000, 3000, 2000, 1000]
-          myBarChart.update()
-      });
+  let getStats = (project, year) => {
+    fetch('/projectStats', {
+      method: 'post',
+      body: JSON.stringify({
+        project: project,
+        year: year
+      }),
+      headers: {
+        'content-type': 'application/json'
+      },
+    }).then(function (response) {
+      return response.json();
+    }).then(function (res) {
+      myBarChart.data.datasets[0].data = res.cabimentoAnual
+      myBarChart.data.datasets[1].data = res.faturadoAnual
+      myBarChart.update()
+    });
+  }
+
+
+});
--- routes/gestao.js
+++ routes/gestao.js
@@ -1,19 +1,75 @@
 "use strict"
 
-var express = require('express');
-var router = express.Router();
-var firebase = require('firebase');
+let express = require('express');
+let router = express.Router();
+let firebase = require('firebase');
 
-var projectsRef = firebase.database().ref("gestãoLIP");
+let projectsRef = firebase.database().ref("gestãoLIP");
 
-router.get("/getProjectsInfo", (req,res)=>{
-  projectsRef.once("value", (data)=>{
+router.get("/getProjectsInfo", (req, res) => {
+  projectsRef.once("value", (data) => {
     res.send({
       data: data.val()
     })
-   },(err)=> {console.log(err)
-   }); 
+  }, (err) => {
+    console.log(err)
+  });
 });
 
+var ref = firebase.database().ref("registoLIP");
+router.post("/projectStats", (req, res) => {
+  let totalCabimentado = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    totalFaturado = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  ref.orderByChild("fundo").equalTo(req.body.project).once("value", (snap) => {
+    var queryData = snap.val();
+    if(queryData != null){
+      totalCabimentado.map((value, month) => {
+        Object.keys(queryData).map((order) => {
+          if (queryData[order].pedidoMes == month + 1 && queryData[order].pedidoAno == req.body.year && queryData[order].cabimentado != "") {
+            totalCabimentado[month] += parseFloat(queryData[order].cabimentado);
+          }
+          if (queryData[order].pedidoMes == month + 1 && queryData[order].pedidoAno == req.body.year && queryData[order].estado == "Anulado") {
+            totalCabimentado[month] -= parseFloat(queryData[order].cabimentado);
+          }
+          if (queryData[order].pedidoMes == month + 1 && queryData[order].pedidoAno == req.body.year && queryData[order].estado == "Feito") {
+            totalCabimentado[month] -= (parseFloat(queryData[order].dif));
+          }
+          //Calculo de total faturado por mes
+          if (queryData[order].faturaMes == month + 1 && queryData[order].faturaAno == req.body.year && queryData[order].faturado != "") {
+            totalFaturado[month] += parseFloat(queryData[order].faturado);
+          }
+        });
+      });
+    }
+
+    res.send({
+      cabimentoAnual: totalCabimentado,
+      faturadoAnual: totalFaturado
+    })
+  });
+
+});
 
 module.exports = router;
+
+
+
+// for (let j = 0; j < totalCabimentado.length; j++) {
+//   for (let i = 0; i < dados.length; i++) {
+//     //Calculo de total cabimentado por mes
+//     if (dados[i].pedidoMes == j + 1 && dados[i].pedidoAno == currentYear && dados[i].cabimentado != "") {
+//       totalCabimentado[j] += parseFloat(dados[i].cabimentado);
+//     }
+//     if (dados[i].pedidoMes == j + 1 && dados[i].pedidoAno == currentYear && dados[i].estado == "Anulado" && dados[i].cabimentado != "") {
+//       totalCabimentado[j] -= parseFloat(dados[i].cabimentado);
+//     }
+//     if (dados[i].pedidoMes == j + 1 && dados[i].pedidoAno == currentYear && dados[i].estado == "Feito" && dados[i].cabimentado != "") {
+//       totalCabimentado[j] -= (parseFloat(dados[i].dif));
+//     }
+
+//     //Calculo de total faturado por mes
+//     if (dados[i].faturaMes == j + 1 && dados[i].faturaAno == currentYear && dados[i].faturado != "") {
+//       totalFaturado[j] += parseFloat(dados[i].faturado);
+//     }
+//   }
+// }
